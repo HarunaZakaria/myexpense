f109634c0c3f9cc048448674bf944b32ed880c8b
Harunzy <Harunzy55@gmail.com>
2026-04-04 11:31:50 -0700

    add

--- js/add.js
+++ js/add.js
@@ -3,12 +3,21 @@ const form = document.getElementById("expForm");
 form.addEventListener("submit", (event) => {
   event.preventDefault(); // Stop page refresh
 
+  const newExpenses = [];
+
   // Get all form data
   const formData = new FormData(event.target);
 
   // Retrieve specific values by name
   const name = formData.get("itemName");
-  const price = formData.get("price");
-  console.log(name);
-  console.log(price);
+  const price = " Ghc" + formData.get("price");
+  newExpenses.push(name + price);
+  console.log(newExpenses);
+  const listContainer = document.querySelector(".list-container");
+
+  newExpenses.forEach((expense) => {
+    const li = document.createElement("li");
+    li.textContent = expense;
+    listContainer.appendChild(li);
+  });
 });
