6980ba11702d09432fc3501928314e00c8e4e408
Harunzy <Harunzy55@gmail.com>
2026-04-05 10:36:56 -0700

    add

--- js/add.js
+++ js/add.js
@@ -2,26 +2,20 @@ const form = document.getElementById("expForm");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault(); // Stop page refresh
-
-  const newExpenses = [];
-
   // Get all form data
   const formData = new FormData(event.target);
-
   // Retrieve specific values by name
-  const name = formData.get("itemName");
-  const price = " Ghc" + formData.get("price");
-  newExpenses.push(name + price);
+
+  const itemName = formData.get("itemName");
+  const itemPrice = " Ghc" + formData.get("itemPrice");
+  const newExpenses = { name: itemName, price: itemPrice };
   console.log(newExpenses);
   const listContainer = document.querySelector(".list-container");
-
-  newExpenses.forEach((expense) => {
-    const li = document.createElement("li");
-    const hr = document.createElement("hr");
-    li.textContent = expense;
-    li.classList.add("exp-list");
-    hr.classList.add("exp-list-hr");
-    listContainer.appendChild(li);
-    listContainer.appendChild(hr);
-  });
+  const li = document.createElement("li");
+  const hr = document.createElement("hr");
+  li.textContent = newExpenses.name + newExpenses.price;
+  li.classList.add("exp-list");
+  hr.classList.add("exp-list-hr");
+  listContainer.appendChild(li);
+  listContainer.appendChild(hr);
 });
